f4b21c85e726a6cbc9d161b9f1f2a11beb4984ab
AmeliaSroczynska <amelia.sroczynska@gmail.com>
2026-05-01 03:25:03 +0200

    feat(backend): add model and initial migration

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    class Unit(models.TextChoices):
+        PIECES = 'PCS', 'kawałków'
+        SLICES = 'SLI', 'plasterków'
+        BOTTLE = 'BTL', 'butelki'
+        CAPSULES = 'CAP', 'kapsułek'
+        CARTON = 'CRT', 'kartonu'
+
+    name = models.CharField(max_length=20)
+    quantity = models.FloatField()
+    slug = models.SlugField(unique=True, blank=True, max_length=20)
+    unit = models.CharField(
+        max_length=3,
+        choices=Unit.choices,
+        default=Unit.PIECES,
+    )
+    minimum_quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
