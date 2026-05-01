1a41e69fdd1cc9022e8d37ba3f15c6fe97ae0c76
AmeliaSroczynska <amelia.sroczynska@gmail.com>
2026-05-02 01:52:22 +0200

    feat(backend): add actions to manage product quantity

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,8 +1,34 @@
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    lookup_field = 'slug'
+
+    @action(methods=['post'], detail=True)
+    def add(self, request, slug=None):
+        product = self.get_object()
+
+        product.quantity += 1
+        product.save()
+
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+    @action(methods=['post'], detail=True)
+    def take(self, request, slug=None):
+        product = self.get_object()
+
+        if product.quantity == 0:
+            return Response({'error': 'Brak produktu na stanie.'}, status=400)
+        else:
+            product.quantity -= 1
+            product.save()
+
+            serializer = ProductSerializer(product)
+            return Response(serializer.data)
