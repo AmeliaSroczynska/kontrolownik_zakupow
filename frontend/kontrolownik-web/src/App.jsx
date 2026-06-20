40f1c34fb7d1ed320f720d05280a994878f73ae3
AmeliaSroczynska <amelia.sroczynska@gmail.com>
2026-06-20 18:39:16 +0200

    chore: eslint clean up code

--- frontend/kontrolownik-web/src/App.jsx
+++ frontend/kontrolownik-web/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainList from './pages/MainList.jsx';
 import ProductDetail from './pages/ProductDetail.jsx';
